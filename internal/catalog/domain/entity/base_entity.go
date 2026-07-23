80163631b16741bc4fc6cbc1149479231254b94e
naina <naina.shambazova@gmail.com>
2026-07-23 15:22:48 +0300

    chore: added entities

---
+++ internal/catalog/domain/entity/base_entity.go
@@ -0,0 +1,8 @@
+package entity
+
+import "github.com/google/uuid"
+
+type BaseEntity struct {
+	ID    uuid.UUID `json:"id"`
+	Title string    `json:"name"`
+}
---
+++ internal/catalog/domain/entity/catalog_item.go
@@ -0,0 +1,11 @@
+package entity
+
+type CatalogItem struct {
+	BaseEntity
+	ShortDescription string
+	FullDescription  string
+	ImageURL         string
+	Brand            *Brand
+	Category         *Category
+	Price            float64
+}
